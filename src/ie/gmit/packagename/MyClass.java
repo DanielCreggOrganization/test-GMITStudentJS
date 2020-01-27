af6d463be6f30282956e3c2fc424eeacce62da02
GMITStudentJS <g00363297@gmit.ie>
2020-01-27 17:47:14 +0000

    classes

---
+++ src/ie/gmit/packagename/MyClass.java
@@ -0,0 +1,10 @@
+package ie.gmit.labweek2;
+
+class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+
+}
---
+++ src/lectureweek3/DataTypes.java
@@ -0,0 +1,24 @@
+package ie.gmit.lectureweek3;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        boolean isAlive = true;
+        byte age = 100;
+        short popgalway = 80000;
+        int studentgmit = 7000;
+        long phonenumber = 35386649376L;
+        float value = 3.33f;
+        double weight = 14.5;
+        char blood = 'B';
+
+        System.out.println(blood);
+        System.out.println(weight);
+        System.out.println(value);
+        System.out.println(phonenumber);
+        System.out.println(studentgmit);
+        System.out.println(popgalway);
+        System.out.println(isAlive);
+        System.out.println(age);
+    }
+}
